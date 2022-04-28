c24417a82e4e3db82d514ce309a47b8f35fa1f41
stepassoni <passonistefano94@gmail.com>
2022-04-28 16:38:24 +0200

    modifica python + risultati

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
 from pyBUMP import *
 
+caso="mesh"
 
-# CASO BASE
-xVars, avgVars = readScalar("bubbleColumnDegass", "surfaces", "alphaMean.air_y_63cm.raw")
-plot(1,xVars, avgVars, "surfaces", 10, 63, lastCompare=False)
-xVars, avgVars = readScalar("bubbleColumnDegass", "surfaces", "alphaMean.air_y_63cm.raw")
-plot(1,xVars, avgVars, "surfaces", 10, 63)
+# CONFRONTO MESH
+if caso=="mesh":       
+    path1="results/run001"
+    path2="results/run002"
+    path3="results/run003"
+    path4="results/run004"
+
+    label1="run 001"
+    label2="run 002"
+    label3="run 003"
+    label4="run 004"
+
+    dt1=0.05
+
+    #plot time-averaged aplha.air superficie 8cm
+    xVars1, avgVars1 = readScalar(path1, "surfaces", "alphaMean.air_y_8cm.raw",nz=4)
+    plot(1,xVars1, avgVars1, "surfaces", 10, 8, label1 ,lastCompare=False)
+    xVars2, avgVars2 = readScalar(path2, "surfaces", "alphaMean.air_y_8cm.raw",nz=8)
+    plot(1,xVars2, avgVars2, "surfaces", 10, 8, label2, lastCompare=False)
+    xVars3, avgVars3 = readScalar(path3, "surfaces", "alphaMean.air_y_8cm.raw",nz=8)
+    plot(1,xVars3, avgVars3, "surfaces", 10, 8, label3)
+    #xVars4, avgVars4 = readScalar(path4, "surfaces", "alphaMean.air_y_8cm.raw",nz=8)
+    #plot(1,xVars4, avgVars4, "surfaces", 10, 8, label4)
 
 
+    #plot time-averaged aplha.air superficie 63cm
+    xVars, avgVars = readScalar(path1, "surfaces", "alphaMean.air_y_63cm.raw",nz=4)
+    plot(2,xVars, avgVars, "surfaces", 10, 63, label1 ,lastCompare=False)
+    xVars, avgVars = readScalar(path2, "surfaces", "alphaMean.air_y_63cm.raw",nz=8)
+    plot(2,xVars, avgVars, "surfaces", 10, 63, label2, lastCompare=False)
+    xVars3, avgVars3 = readScalar(path3, "surfaces", "alphaMean.air_y_63cm.raw",nz=8)
+    plot(2,xVars, avgVars, "surfaces", 10, 63, label3)
+    #xVars, avgVars = readScalar(path4, "surfaces", "alphaMean.air_y_63cm.raw",nz=8)
+    #plot(2,xVars4, avgVars4, "surfaces", 10, 63, label4)
 
-xHold, yHold = readScalar("bubbleColumnDegass", "holdUp", "volFieldValue.dat")
-plot(2,xHold, yHold, "holdUp", 10, 8)
 
-xHold, yHold = readScalar("bubbleColumnDegass", "holdUp", "volFieldValue.dat", instantaneous=True)
-plot(3,xHold, yHold, "holdUpInstantaneous", 10, 8)
+    #plot media holdup da 30s
+    xHold, yHold = readScalar(path1, "holdUp", "volFieldValue.dat", dt=dt1)
+    plot(3,xHold, yHold, "holdUp", 10, 8, label1,lastCompare=False)
+    xHold, yHold = readScalar(path2, "holdUp", "volFieldValue.dat", dt=dt1)
+    plot(3,xHold, yHold, "holdUp", 10, 8, label2,lastCompare=False)
+    xHold, yHold = readScalar(path3, "holdUp", "volFieldValue.dat", dt=dt1)
+    plot(3,xHold, yHold, "holdUp", 10, 8, label3)
+    #xHold, yHold = readScalar(path3, "holdUp", "volFieldValue.dat", dt=dt1)
+    #plot(3,xHold, yHold, "holdUp", 10, 8, label4)
+
+    #plot holdup istantaneo
+    xHold, yHold = readScalar(path1, "holdUp", "volFieldValue.dat", instantaneous=True)
+    plot(4,xHold, yHold, "holdUpInstantaneous", 10, 8, label1,lastCompare=False)
+    xHold, yHold = readScalar(path2, "holdUp", "volFieldValue.dat", instantaneous=True)
+    plot(4,xHold, yHold, "holdUpInstantaneous", 10, 8, label2,lastCompare=False)
+    xHold, yHold = readScalar(path3, "holdUp", "volFieldValue.dat", instantaneous=True)
+    plot(4,xHold, yHold, "holdUpInstantaneous", 10, 8, label3)
+    #xHold, yHold = readScalar(path4, "holdUp", "volFieldValue.dat", instantaneous=True)
+    #plot(4,xHold, yHold, "holdUpInstantaneous", 10, 8, label4)
+
+elif caso=="time":
+    print("ciao")    
--- pyBUMP.py
+++ pyBUMP.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import os
 
-def readScalar(caseFolder, sampleType, fileName, instantaneous=False):
+def readScalar(caseFolder, sampleType, fileName, nz=4, instantaneous=False, dt=0.05):
     
     #Find sample path
     sampleTypePath = os.path.join(caseFolder, "postProcessing", sampleType)
@@ -21,19 +21,19 @@ def readScalar(caseFolder, sampleType, fileName, instantaneous=False):
         vars = vars[np.argsort(vars[:, 0])]
         
         #Mean over equal x values
-        yVars = np.average(vars[:,3].reshape(-1, 4), axis=1)
+        yVars = np.average(vars[:,3].reshape(-1, nz), axis=1)
         
         #x values
-        xVars = vars[::4, 0]
+        xVars = vars[::nz, 0]
         
     elif sampleType=="holdUp":
         if instantaneous==True:
             xVars = vars[:,0]
             yVars = vars[:,1]
         else:
-            xVars = vars[:,0]
-            yVars = cum_mean(vars[:,1])
-    
+            xVars = vars[int(30/dt)::,0]
+            yVars = cum_mean(vars[int(30/dt)::,1])          
+            #print("Time-averaged gas holdup = " + str(yVars[-1]))
     return xVars, yVars
 
 def readExpData(fileName):
@@ -79,9 +79,16 @@ def cum_mean(arr):
     
     return cum_sum
 
-
-def plot(figID, xVars, yVars, sampleType, J, h, lastCompare=True):
+def write_holdup(yVars,save_path):
+    with open(save_path+"global_holdup.txt",'a+'):
+        pass
     
+
+def plot(figID, xVars, yVars, sampleType, J, h, label, caso="mesh", lastCompare=True):
+    
+    save_path=os.path.join("./graphs", caso)
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
     # Import experimental data
     expDataDict = readExpData("Krepper_expData.xlsx")
     expData = expDataDict['J'+str(J)+'h'+str(h)]
@@ -91,38 +98,43 @@ def plot(figID, xVars, yVars, sampleType, J, h, lastCompare=True):
     
     if sampleType=="surfaces":   
         xNorm = xVars/max(xVars)
-        plt.plot(xNorm, yVars, 'o', label = r"simulation - $J_G$ = "+str(J)+" mm/s", linestyle="--")
+        plt.plot(xNorm, yVars, 'o', label = label + r" - $J_G$ = "+str(J)+" mm/s", linestyle="--")
         plt.title('Gas volume fraction distribution at h = '+str(h)+' cm')
         plt.xlabel('x/L (-)')
         plt.ylabel('gas volume fraction (-)')
         plt.grid(which='both', alpha=0.3)
-        plt.xlim(0,1.025)
-        plt.ylim(0.03,0.055)
+        if h==8:
+            plt.xlim(0,1.025)
+            plt.ylim(0,0.06)
+        if h==63:
+            plt.xlim(0,1.025)
+            plt.ylim(0.03,0.055)
         plt.xticks(np.arange(0,1.025,0.1))
         if lastCompare==True:
             plt.plot(expData[:,1], expData[:,2], 'o', label=r"experimental - $J_G$ = "+str(J)+" mm/s", linestyle="--")
             plt.legend()
-            plt.savefig('surfacesJ'+str(J)+'h'+str(h)+'.png')
+            plt.savefig(save_path+'/surfacesJ'+str(J)+'h'+str(h)+'.png')
+            plt.close() 
         
     elif sampleType=="holdUp":
-        plt.plot(xVars, yVars, label = r"$J_G$ = "+str(J)+" mm/s")
+        plt.plot(xVars, yVars, label = label + r" - $J_G$ = "+str(J)+" mm/s")
         if lastCompare==True:
             plt.legend()
             plt.title('Global gas holdup')
             plt.xlabel('Time (s)')
             plt.ylabel('gas holdup (-)')
-            plt.grid(which='both', alpha=0.3)   
-        plt.savefig('holdUp'+str(J)+'.png')
+            plt.grid(which='both', alpha=0.3)  
+        plt.savefig(save_path+'/holdUp'+str(J)+'.png')
         
     elif sampleType=="holdUpInstantaneous":
-        plt.plot(xVars, yVars, label = r"$J_G$ = "+str(J)+" mm/s")
+        plt.plot(xVars, yVars, label = label + r" - $J_G$ = "+str(J)+" mm/s")
         if lastCompare==True:
             plt.legend()
             plt.title('Instantaneous global gas holdup')
             plt.xlabel('Time (s)')
             plt.ylabel('gas holdup (-)')
             plt.grid(which='both', alpha=0.3)   
-        plt.savefig('holdUpInstantaneous'+str(J)+'.png')
+        plt.savefig(save_path+'/holdUpInstantaneous'+str(J)+'.png')
         
     else:
         print("Error! no such sample type!")
